3cb27148462a1d1542fbd4e432f9f5863e38d95a
Dmitri Tsornoi <dmitri.tsornoi@gmail.com>
2021-05-07 21:19:50 +0300

    feat: Added LoggingAspect

---
+++ src/main/java/com/agregating/events/aspect/LoggingAspect.java
@@ -0,0 +1,50 @@
+package com.agregating.events.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    private Logger logger = Logger.getLogger(getClass().getName());
+
+    @Pointcut("execution(* com.agregating.events.controller.*.*(..))")
+    private void forControllerPackage(){}
+
+    @Pointcut("execution(* com.agregating.events.repository.*.*(..))")
+    private void forRepositoryPackage(){}
+
+    @Pointcut("execution(* com.agregating.events.service.*.*(..))")
+    private void forServicePackage(){}
+
+    @Pointcut("forRepositoryPackage() || forServicePackage() || forControllerPackage()")
+    private void forAppFlow(){}
+
+    @Before("forAppFlow()")
+    public void before(JoinPoint joinPoint){
+        String method = joinPoint.getSignature().toShortString();
+
+        logger.info("@Before: calling method: " + method);
+
+        Object[] args = joinPoint.getArgs();
+
+        for (Object argument: args){
+            logger.info("argument: " + argument);
+        }
+    }
+
+    @AfterReturning(pointcut = "forAppflow()", returning = "result")
+    public void afterReturning(JoinPoint joinPoint, Object result){
+        String method = joinPoint.getSignature().toShortString();
+        logger.info("@AfterReturning: from method: " + method);
+
+        logger.info("result: " + result);
+    }
+}
