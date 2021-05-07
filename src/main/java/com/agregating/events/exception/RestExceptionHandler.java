f342e4a47e3b531be3cb0e52f59e970dee554597
Dmitri Tsornoi <dmitri.tsornoi@gmail.com>
2021-05-07 21:19:30 +0300

    feat: Added Exception handling

---
+++ src/main/java/com/agregating/events/exception/RestExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.agregating.events.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleException(Exception exception){
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                exception.getMessage(),
+                System.currentTimeMillis()
+        );
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+}
