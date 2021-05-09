a8301ee7d6ebe359a43e5f0123427939dbbb144d
DMITRI TŠORNÕI <47533288+dtsornoi@users.noreply.github.com>
2021-05-09 20:02:58 +0300

    test: UserRestController test

---
+++ src/test/java/com/agregating/events/controller/UserRestControllerTest.java
@@ -0,0 +1,54 @@
+package com.agregating.events.controller;
+
+import com.agregating.events.domain.User;
+import com.agregating.events.repository.UserRepository;
+import com.agregating.events.service.CustomUserService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+
+/**
+ * Tests for UserRestController
+ *
+ * @author Dmitri Tšornõi
+ */
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+class UserRestControllerTest {
+
+    @Mock
+    private CustomUserService service;
+
+    @InjectMocks
+    private UserRestController controller;
+
+    @Test
+    @DisplayName("When findById() is called, return User with requested id and status 200")
+    void whenFindByIdCalled_returnUserWithRequestedId() throws Exception {
+        User mockUser = new User();
+        mockUser.setId(1L);
+        mockUser.setUsername("mockUser");
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        Mockito.when(service.findById(Mockito.anyLong())).thenReturn(Optional.of(mockUser));
+        ResponseEntity<User> responseEntity = controller.findById(1L);
+
+        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(200);
+        assertThat(responseEntity.getBody()).isEqualTo(mockUser);
+    }
+}
