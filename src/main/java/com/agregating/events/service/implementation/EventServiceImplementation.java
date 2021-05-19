521c03be0399ec86a01afbd697a2cae96d8d1019
Dmitri Tsornoi <dmitri.tsornoi@gmail.com>
2021-05-19 20:26:11 +0300

    rebuild

--- src/main/java/com/agregating/events/service/implementation/EventServiceImplementation.java
+++ src/main/java/com/agregating/events/service/implementation/EventServiceImplementation.java
@@ -109,6 +109,6 @@ public class EventServiceImplementation implements EventService {
         List<User> users = event.getSubscribedUsers();
         users.remove(user);
         event.setSubscribedUsers(users);
-        return updateEvent(id, event);
+        return updateEvent(id, event);§
     }
 }
